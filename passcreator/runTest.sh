b62a30e2679f71eea978cbd6489e7b8a301165ae
Philipp von dem Bussche <philipp.bussche@gmail.com>
2016-08-21 17:59:17 +0200

    A first test

---
+++ passcreator/runTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd $HOME/python-virtualenvs/passgenerator/
+source bin/activate
+cd $HOME/IdeaProjects/passcreator
+$HOME/python-virtualenvs/passgenerator/bin/python test.py
---
+++ passcreator/test.py
@@ -0,0 +1,17 @@
+from wallet.models import Pass, Barcode, StoreCard
+
+cardInfo = StoreCard() 
+cardInfo.addPrimaryField('name', 'John Doe', 'Name')
+
+organizationName = 'Phils Pass Generator' 
+passTypeIdentifier = 'pass.de.mobilcom.25jahre' 
+teamIdentifier = 'N642X794C7'
+
+passfile = Pass(cardInfo, passTypeIdentifier=passTypeIdentifier, organizationName=organizationName, teamIdentifier=teamIdentifier)
+passfile.serialNumber = '1234567'
+passfile.barcode = Barcode(message = 'Barcode message')
+
+# Including the icon and logo is necessary for the passbook to be valid. 
+passfile.addFile('icon.png', open('images/icon.png', 'r')) 
+passfile.addFile('logo.png', open('images/logo.png', 'r')) 
+passfile.create('certificate.pem', 'key.pem', 'wwdr.pem', 'test', 'test.pkpass') # Create and output the Passbook file (.pkpass)
