5d3f43193e427eb9ee37e8f8e8107fc00d053368
Philipp von dem Bussche <philipp.bussche@gmail.com>
2016-08-21 18:05:13 +0200

    Writing passes into a passes directory

--- test.py
+++ test.py
@@ -14,4 +14,4 @@ passfile.barcode = Barcode(message = 'Barcode message')
 # Including the icon and logo is necessary for the passbook to be valid. 
 passfile.addFile('icon.png', open('images/icon.png', 'r')) 
 passfile.addFile('logo.png', open('images/logo.png', 'r')) 
-passfile.create('certificate.pem', 'key.pem', 'wwdr.pem', 'test', 'test.pkpass') # Create and output the Passbook file (.pkpass)
+passfile.create('certificate.pem', 'key.pem', 'wwdr.pem', 'test', 'passes/test.pkpass') # Create and output the Passbook file (.pkpass)
